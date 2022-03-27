9321d9a0859f1684d12777179e3a4e43b27886a7
afif <afif@diigtalamoeba.id>
2022-03-28 01:11:43 +0700

    upload

---
+++ app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		echo view('template/header');
+		echo view('layout/home');
+		echo view('template/footer');
+	}
+}
